d674ac37fb4e9ba4b4beb4fefbda0eb8d9a0b320
kosiorro <37558883+kosiorro@users.noreply.github.com>
2026-09-24 16:17:37 +0200

    Fix authentication gate wiring

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,17 +4,10 @@ import { EuiErrorBoundary } from '@elastic/eui';
 import { Global } from '@emotion/react';
 import Head from 'next/head';
 
-import Chrome from '../components/chrome';\nimport AuthGate from '../components/AuthGate';
+import AuthGate from '../components/AuthGate';
+import Chrome from '../components/chrome';
 import { Theme } from '../components/theme';
 import { globalStyles } from '../styles/global.styles';
-
-/**
- * Next.js uses the App component to initialize pages. You can override it
- * and control the page initialization. Here use use it to render the
- * `Chrome` component on each page, and apply an error boundary.
- *
- * @see https://nextjs.org/docs/advanced-features/custom-app
- */
 
 const EuiApp = ({ Component, pageProps }) => {
   return (
@@ -26,11 +19,14 @@ const EuiApp = ({ Component, pageProps }) => {
       <Theme>
         <Chrome>
           <EuiErrorBoundary>
-            <Component {...pageProps} />
+            <AuthGate>
+              <Component {...pageProps} />
+            </AuthGate>
           </EuiErrorBoundary>
         </Chrome>
       </Theme>
     </>
   )
 }
+
 export default EuiApp
